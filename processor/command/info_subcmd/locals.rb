ceb27459bfd6cc7d0d0ca6fc0fd33d2e90c8d0b4
rocky <rockyb@rubyforge.org>
2011-06-18 12:59:24 -0400

    Add 'info iv' for instance variables. Use better way to suppress warnings.

--- processor/command/info_subcmd/locals.rb
+++ processor/command/info_subcmd/locals.rb
@@ -4,9 +4,10 @@ require 'rubygems'; require 'require_relative'
 require 'columnize'
 require_relative '../base/subcmd'
 require_relative '../../../app/frame'
+require_relative '../../../app/util'
 
 class Trepan::Subcommand::InfoLocals < Trepan::Subcommand
-  unless defined?(HELP)
+  Trepan::Util.suppress_warnings {
     Trepanning::Subcommand.set_name_prefix(__FILE__, self)
     HELP         = <<-EOH
 #{CMD}
@@ -21,22 +22,22 @@ EOH
     MAX_ARGS     = 1
     MIN_ABBREV   = 'lo'.size 
     NEED_STACK   = true
-  end
+  }
 
-  def get_local_names
+  def get_names
     @proc.frame.local_variables.keys
   end
 
-  def run(args)
+  def run_for_type(args, type)
     if args.size == 3
       if 0 == 'names'.index(args[-1].downcase)
-        local_names = get_local_names()
-        if local_names.empty?
-            msg "No local variables defined."
+        names = get_names()
+        if names.empty?
+          msg "No #{type} variables defined."
         else
-          section "Local variable names:"
+          section "#{type.capitalize} variable names:"
           width = settings[:maxwidth]
-          mess = Columnize::columnize(local_names, 
+          mess = Columnize::columnize(names, 
                                       @proc.settings[:maxwidth], '  ',
                                       false, true, ' ' * 2).chomp
           msg mess
@@ -45,18 +46,23 @@ EOH
         errmsg("unrecognized argument #{args[2]}")
       end
     elsif args.size == 2
-      local_names = get_local_names
-      if local_names.empty?
-        msg "No local variables defined."
+      names = get_names
+      if names.empty?
+        msg "No #{type} variables defined."
       else
-        section "Local variables:"
-        @proc.frame.local_variables.each do |var_name, var_value| 
+        section "#{type.capitalize} variables:"
+        get_names.each do |var_name| 
+          var_value = @proc.safe_rep(@proc.debug_eval_no_errmsg(var_name).inspect)
           msg("#{var_name} = #{var_value}")
         end
       end
     else
       errmsg("Wrong number of arguments #{args.size}")
     end
+  end
+
+  def run(args)
+    run_for_type(args, 'local')
   end
 end
 
